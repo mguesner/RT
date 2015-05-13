15d51082e1e70862ce97c1fead75316c6e1e09cf
Benoit SOURD-BLEVIN <benoit.sourd@gmail.com>
2015-05-13 13:18:10 +0200

    merge

---
+++ src/csg/merge.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   merge.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsourd-b <bsourd-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/13 13:01:44 by bsourd-b          #+#    #+#             */
+/*   Updated: 2015/05/13 13:06:58 by bsourd-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <rt.h>
+
+double	dist_merge(t_obj *obj1, t_obj *obj2, t_vec *vec, t_point *o)
+{
+	double	dist1;
+	double	dist2;
+
+	dist1 = touch(obj1, vec, o);
+	dist2 = touch(obj2, vec, o);
+	if (dist1 > 0 && dist2 < 0)
+		return (dist1);
+	if (dist2 > 0 && dist1 < 0)
+		return (dist2);
+	return (-1);
+
+}
