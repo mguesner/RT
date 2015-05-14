765f1928aebacc4f1f367a2e8eb16ff9e14f2a4b
Benoit SOURD-BLEVIN <benoit.sourd@gmail.com>
2015-05-14 15:04:05 +0200

    set light

---
+++ src/set_color_light.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_color_light.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsourd-b <bsourd-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/14 12:55:34 by bsourd-b          #+#    #+#             */
+/*   Updated: 2015/05/14 14:45:40 by bsourd-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <rt.h>
+
+static int	up_color(int obj_color, int cur_color, double dist, double power)
+{
+	int	ret;
+
+	ret = (power * obj_color / dist);
+	if (cur_color + ret > obj_color)
+		return (obj_color);
+	if (cur_color + ret > obj_color)
+		return (0);
+	return (cur_color + ret);
+}
+
+void		set_color_light(t_obj *light, t_pix *pix, t_point inter)
+{
+	t_vec	dir;
+	double	light_dist;
+
+	dir = make_vec(light->coord, inter);
+	light_dist = norme(dir);
+	dir = normalize(dir);
+	pix->color->b = up_color(pix->cur_obj->color.b, pix->color->b, light_dist, 1000);
+	pix->color->g = up_color(pix->cur_obj->color.g, pix->color->g, light_dist, 1000);
+	pix->color->r = up_color(pix->cur_obj->color.r, pix->color->r, light_dist, 1000);
+}
