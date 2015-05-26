80dd6b1ac4c10765af2223a655dbfc1c460bc0a2
Matthieu GUESNEROT <mguesner@e1r10p4.42.fr>
2015-05-26 15:38:01 +0200

    fsd

--- src/cylinder.c
+++ src/cylinder.c
@@ -6,12 +6,22 @@
 /*   By: mguesner <mguesner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/12 11:20:04 by bsourd-b          #+#    #+#             */
-/*   Updated: 2015/05/26 14:26:09 by mguesner         ###   ########.fr       */
+/*   Updated: 2015/05/26 15:37:09 by mguesner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rt.h>
 #include <matrice.h>
+
+double	dist_cylinder3(double det, double a, double b)
+{
+	double	ret;
+	double	ret2;
+
+	ret = (-b + sqrt(det)) / (2.0 * a);
+	ret2 = (-b - sqrt(det)) / (2.0 * a);
+	return (ret2 > 0.0001 ? ret2 : ret);
+}
 
 double	dist_cylinder2(t_cylinder *cylinder, t_vec *v, t_point o)
 {
@@ -29,34 +39,26 @@ double	dist_cylinder2(t_cylinder *cylinder, t_vec *v, t_point o)
 		, 2) - pow(cylinder->radius, 2);
 	det = pow(b, 2) - 4.0 * a * c;
 	if (det > 0)
-	{
-		double ret = (-b + sqrt(det)) / (2.0 * a);
-		double ret2 = (-b - sqrt(det)) / (2.0 * a);
-		return (ret2 > 0.0001 ? ret2 : ret);
-	}
+		return (dist_cylinder3(det, a, b));
 	return (-1);
 }
 
 double	dist_cylinder(t_cylinder *cylinder, t_vec *v, t_point *ori)
 {
-	double	a;
-	double	b;
-	double	c;
+	double	param[3];
 	double	det;
 	t_point	vec;
 	t_point	o;
 
 	vec = do_rotate(cylinder->rot, *(t_point *)v);
 	o = do_rotate(cylinder->rot, *ori);
-	a = pow(vec.x, 2) + pow(vec.y, 2);
-	b = 2.0 * (vec.x * (o.x - cylinder->coord.x) + vec.y * (o.y - cylinder->coord.y));
-	c = pow((o.x - cylinder->coord.x), 2) + pow((o.y - cylinder->coord.y), 2) - pow(cylinder->radius, 2);
-	det = pow(b, 2) - 4.0 * a * c;
+	param[0] = pow(vec.x, 2) + pow(vec.y, 2);
+	param[1] = 2.0 * (vec.x * (o.x - cylinder->coord.x)
+		+ vec.y * (o.y - cylinder->coord.y));
+	param[2] = pow((o.x - cylinder->coord.x), 2)
+	+ pow((o.y - cylinder->coord.y), 2) - pow(cylinder->radius, 2);
+	det = pow(param[1], 2) - 4.0 * param[0] * param[2];
 	if (det > 0)
-	{
-		double ret = (-b + sqrt(det)) / (2.0 * a);
-		double ret2 = (-b - sqrt(det)) / (2.0 * a);
-		return (ret2 > 0.0001 ? ret2 : ret);
-	}
+		return (dist_cylinder3(det, param[0], param[1]));
 	return (-1);
 }
